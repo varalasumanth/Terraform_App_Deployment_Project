7d5cb81e1200f41647de23f0943810dca72bc1dc
varalasumanth <sumanth.varala8056@gmail.com>
2023-09-20 08:04:42 +0000

    Application lb and s3 backend setup code

---
+++ elb.tf
@@ -0,0 +1,91 @@
+/*resource "aws_elb" "elb" {
+  name               = "web-elb"
+  vpc_id             = aws_vpc.vpc.id
+  availability_zones = ["us-east-1a", "us-east-1b"]
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 5
+    timeout             = 5
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  instances                 = ["${aws_instance.webserver1.id}", "${aws_instance.webserver2.id}"]
+  cross_zone_load_balancing = true
+  idle_timeout              = 400
+  tags = {
+    Name = "web-elb"
+  }
+}
+
+output "loadbalancerdns" {
+  value = aws_elb.elb.dns_name
+}*/
+
+#create alb
+resource "aws_lb" "myalb" {
+  name               = "myalb"
+  internal           = false
+  load_balancer_type = "application"
+
+  security_groups = [aws_security_group.websg.id]
+  subnets         = [aws_subnet.sub1.id, aws_subnet.sub2.id]
+
+  tags = {
+    Name = "web"
+  }
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "myTG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+
+  health_check {
+    path = "/"
+    port = "traffic-port"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "attach1" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.webserver1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "attach2" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.webserver2.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.myalb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.tg.arn
+    type             = "forward"
+  }
+}
+
+output "loadbalancerdns" {
+  value = aws_lb.myalb.dns_name
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sumanth8056"
+    key    = "sumanth/prod"
+    region = "us-east-1"
+  }
+}
